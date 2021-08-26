c9327f63c5dbaf53c7bbc270d3b447de8e1ef51f
Miguel22247 <nr2862002@gmail.com>
2021-08-25 21:07:06 -0300

    fix pep8

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -9,11 +9,14 @@ env.hosts = ['34.139.167.198', '34.138.129.5']
 
 
 def do_pack():
+    
+    format_d = "%Y%m%d%H%M%S"
+    cmd = "mkdir -p versions"
     """Generates a .tgz archive"""
     try:
-        local("mkdir -p versions")
-        n = datetime.now()
-        filename = "versions/web_static_" + n.strftime("%Y%m%d%H%M%S") + ".tgz"
+        local(cmd)
+        date = datetime.now()
+        filename = "versions/web_static_" + date.strftime(format_d) + ".tgz"
         local("tar -cvzf " + filename + " web_static")
         return filename
     except:
