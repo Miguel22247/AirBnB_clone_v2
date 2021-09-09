ee9c746955e96a62695d525a02e0a21f01bd9227
Miguel22247 <minipachru@gmail.com>
2021-09-09 13:26:40 -0300

    add import

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Script that prepares a Flask web app"""
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__, template_folder="templates")
 app.url_map.strict_slashes = False
 
