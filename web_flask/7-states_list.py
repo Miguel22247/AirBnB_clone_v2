5c7aa77961bfa16865340ee5113dfb4b0121dacf
Miguel22247 <minipachru@gmail.com>
2021-09-09 15:17:31 -0300

    fix comments

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -14,7 +14,7 @@ def states_list():
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """Remove the current SQLAlchemy Session"""
+    """Remove the current SQLAlchemy"""
     storage.close()
 
 if __name__ == "__main__":
