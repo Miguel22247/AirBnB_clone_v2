57cd345696d01a1aabb093c6f164196934c8a56b
Miguel22247 <minipachru@gmail.com>
2021-09-09 15:20:44 -0300

    changed import

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,14 +2,14 @@
 """Script that list all states"""
 from flask import Flask, render_template
 from models import storage
-from models import State
+
 app = Flask(__name__, template_folder="templates")
 app.url_map.strict_slashes = False
 
 @app.route('/states_list')
 def states_list():
 	"""displays a new HTML page"""
-	new_dict = storage.all(State)
+	new_dict = storage.all('State')
 	return render_template('7-states_list.html', states=new_dict)
 
 @app.teardown_appcontext
