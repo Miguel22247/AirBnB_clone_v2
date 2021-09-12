e97b1ba7f2953a7b515435df573379e764a41228
Miguel Pacheco <minipachru@gmail.com>
2021-09-12 20:53:22 -0300

    fixed?

--- models/state.py
+++ models/state.py
@@ -1,25 +1,16 @@
 #!/usr/bin/python3
 """Defines the State class."""
-import models
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship, backref
 import os
-from os import getenv
-from models.base_model import Base
-from models.base_model import BaseModel
+import models
 from models.city import City
-from sqlalchemy import Column
-from sqlalchemy import String
-from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """Represents a state for a MySQL database.
-    Inherits from SQLAlchemy Base and links to the MySQL table states.
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store States.
-        name (sqlalchemy String): The name of the State.
-        cities (sqlalchemy relationship): The State-City relationship.
-    """
-        __tablename__ = "states"
+    """ State class """
+    __tablename__ = "states"
     if os.getenv('HBNB_TYPE_STORAGE') == "db":
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state", cascade="all, delete")
